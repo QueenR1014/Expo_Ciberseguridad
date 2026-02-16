6f956113a39ec4864d0f98eda66bec28dd2dc852
QueenR1014 <juanreinareyes@hotmail.com>
2026-02-15 22:59:39 -0500

    falta encriptar

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
+import bcrypt from 'bcrypt'
+
 const submit = document.getElementById("signed");
-const login = document.querySelector(".login-container")
+const login = document.querySelector(".login-container");
+const cypher_result = document.querySelector(".cypher-result");
 
 if(submit instanceof HTMLButtonElement){
 
   submit.addEventListener('click', () =>{
+    const cypher = document.getElementById("result");
+    
 
-    login?.classList.toggle("move-left");
+    
+    cypher_result?.classList.toggle("show");
+
   });
 }
 
